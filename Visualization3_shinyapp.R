c4d34b2c7cac9dfbd17598cbb1160c2f7f172f79
Betty Pu <bpu@smith.edu>
2024-03-13 15:38:53 -0400

    changed shiny output format

--- Visualization3_shinyapp.R
+++ Visualization3_shinyapp.R
@@ -1,5 +1,50 @@
 # Visualization 3 - Average Percentage of Digital Payments Across Regions and Income Groups
 
+# Import the two datasets: DatabankWide and df_gdpPC
+df_findex <- read_excel("DatabankWide.xlsx")
+df_gdp <- read.csv("df_gdpPC.csv")
+
+# Select the variables of interest
+df_findex_var <- df_findex[c("Country name", "Year", "Region", "Adult populaiton", "Income group", "Made or received a digital payment (% age 15+)", "Financial institution account (% age 15+)")]
+df_gdp_var <- df_gdp[c("country", "year", "gdpPercap", "lifeExp")]
+
+# Renmae variables
+# Create a new dataframe df_rename
+df_findex_new <- df_findex_var
+
+# Rename "Made or received a digital payment (% age 15+)" to "payment_digital"
+colnames(df_findex_new)[colnames(df_findex_new) == "Made or received a digital payment (% age 15+)"] <- "payment_digital"
+
+# Rename "Financial institution account (% age 15+)" to "account_fin"
+colnames(df_findex_new)[colnames(df_findex_new) == "Financial institution account (% age 15+)"] <- "account_fin"
+
+# Rename "Country name" to "country"
+colnames(df_findex_new)[colnames(df_findex_new) == "Country name"] <- "country"
+
+# Rename "Adult populaiton" to "population"
+colnames(df_findex_new)[colnames(df_findex_new) == "Adult populaiton"] <- "population"
+
+# Rename "Income group" to "group_income"
+colnames(df_findex_new)[colnames(df_findex_new) == "Income group"] <- "group_income"
+
+# Rename "Year" to "year"
+colnames(df_findex_new)[colnames(df_findex_new) == "Year"] <- "year"
+
+# Rename "Region" to "region"
+colnames(df_findex_new)[colnames(df_findex_new) == "Region"] <- "region"
+
+# Remove N/A values
+df_findex_new <- na.omit(df_findex_new)
+
+# Keep observations from years 2014, 2017, and 2021
+df_gdp_var_limit <- subset(df_gdp_var, year %in% c(2014, 2017, 2021))
+df_findex_limit <- subset(df_findex_new, year %in% c(2014, 2017, 2021))
+
+# Merge the two datasets by 'country' and 'year'
+merged_data <- merge(df_findex_limit, df_gdp_var_limit, by = c("year", "country"))
+
+
+
 # Add a new column with corresponding regions
 library(countrycode)
 merged_data$better_region <- countrycode(merged_data$country, origin = "country.name", destination = "region")
@@ -9,9 +54,10 @@ heatmap_data <- merged_data %>%
   group_by(better_region, group_income) %>%
   summarize(mean_payment_digital = mean(payment_digital, na.rm = TRUE), .groups = "drop")
 
+
+
 # Define UI
 ui <- fluidPage(
-  titlePanel("Mean Digital Payments by Region and Income Group"),
   sidebarLayout(
     sidebarPanel(
       selectInput("year", "Select Year", choices = c(2014, 2017, 2021), selected = 2014)
